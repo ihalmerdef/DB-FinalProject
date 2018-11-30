e3286e9414b7bb12e4edbc7a0816e58b1acddb53
Mustafa Alsoalmi <malsolami@luc.edu>
2018-11-29 18:29:08 -0600

    added conntect to the database through mysql-connector

--- CompanyApp/__init__.py
+++ CompanyApp/__init__.py
@@ -1,19 +1,40 @@
 from flask import Flask
+import mysql.connector
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from mysql.connector import Error
+
+# Database connection parameters
+host = 'dbmysql.cpbuyejbc4kx.us-west-2.rds.amazonaws.com'
+database = 'FoodDB'
+user = 'mustafa'
+password = 'mustafa_DBMySQL'
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://mustafa:mustafa_DBMySQL@dbmysql.cpbuyejbc4kx.us-west-2.rds.amazonaws.com:3306/FoodDB'
-print("Success Connection")
-#db = SQLAlchemy(app)
-#bcrypt = Bcrypt(app)
-#login_manager = LoginManager(app)
-#login_manager.login_view = 'login'
-#login_manager.login_message_category = 'info'
 
-#from CompanyApp import routes
-#from CompanyApp import models
+print("Connection via SQLALCHEMY")
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from CompanyApp import routes
+from CompanyApp import models
 
 #models.db.create_all()
+
+def connect():
+	""" Connect to MySQL database """
+	try:
+		conn = mysql.connector.connect(host = host,database = database, user = user, password = password)
+		if conn.is_connected():
+			print('Connected to MySQL database via Mysql Connector')
+	except Error as e:
+		print(e) 
+	finally:
+		conn.close()
+connect()
