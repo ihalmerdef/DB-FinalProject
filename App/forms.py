35a0770a2240f859d37040b49a16bfd901899b10
ilayami <ialyami@luc.edu>
2018-12-06 00:44:34 -0600

    anything

--- App/forms.py
+++ App/forms.py
@@ -5,7 +5,7 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextA
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError,Regexp
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from App import db
-from App.models import Address, Customer, Favorite_List, Label, MenuItem,Favorite_List, Menu, Restaurant, RestaurantOwner, Review, Restaurant_Label
+from App.models import Address, Customer, Label, MenuItem,FavoriteList, Menu, Restaurant, RestaurantOwner, Review, Restaurant_Label,Restaurant_FavoriteList
 from wtforms.fields.html5 import DateField
 
 class RegistrationForm(FlaskForm):
